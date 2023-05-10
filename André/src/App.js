7c6e3478c41cfc2d6e21d28d426c459fe3ab0f61
guilhermesegura <guilhermesegura26@gmail.com>
2023-05-09 22:23:08 -0300

    arruma app.js

--- André/src/App.js
+++ André/src/App.js
@@ -93,36 +93,6 @@ function App() {
 
 // start();
 
-var express = require("express");
-var path = require("path");
-require("express-async-errors");
 
-const connectDB = require("./db/connect");
-require("dotenv").config();
-
-var TeachersRoutes = require("./routes/teachers");
-var ClassRoutes = require("./routes/classes");
-var StudentRoutes = require("./routes/students");
-var UserRoutes = require("./routes/user.js");
-var app = express();
-
-app.use(express.json());
-
-app.use("/api/v1/teachers", TeachersRoutes);
-app.use("/api/v1/classes", ClassRoutes);
-app.use("/api/v1/students", StudentRoutes);
-app.use("/api/v1/users", UserRoutes);
-
-const port = process.env.PORT || 3000;
-const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
-    app.listen(port, () => console.log(`Server is listening port ${port}`));
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-start();
 
 export default App;
