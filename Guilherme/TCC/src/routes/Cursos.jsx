1b87aeb1ed78c560945706acc044a6b5fa403201
Guilherme Segura <guilhermesegura26@gmail.com>
2023-05-18 12:03:02 -0300

    organiza e arruma problemas do front

---
+++ Guilherme/TCC/src/routes/Cursos.jsx
@@ -0,0 +1,44 @@
+import blogFetch from "../axios/config"
+
+import axios from "axios"
+import { useState, useEffect } from "react"
+import "./Cursos.css"
+import { Link } from "react-router-dom"
+
+function Cursos() {
+
+  const[posts, setPosts] = useState([])
+
+  const getPosts = async()=>{
+    try {
+      const response = await blogFetch.get('/posts')
+      const data = response.data
+
+      setPosts(data)
+
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  useEffect(()=> {
+    getPosts()
+  },[])
+
+  return (
+    <div>
+      <h1>Aulas Disponíveis</h1>
+      {posts.length === 0 ? (<p>Nenhuma Aula disponível</p>): (
+        posts.map((post)=>(
+          <div className="post" key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+          <Link to={`/posts/${post.id}`} className="btn">Ler mais</Link>
+          </div>
+        ))
+      )}
+    </div>
+  )
+}
+
+export default Cursos
