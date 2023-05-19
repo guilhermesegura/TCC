11e0dcb3fe62b851b6ec93e3ad201cfeb7e8ef11
anguecalmon <calmon.principal@gmail.com>
2023-05-19 17:10:13 -0300

    Corrige a orientação dos elementos CSS na página home e sobre

--- Guilherme/TCC2/src/routes/Home.jsx
+++ Guilherme/TCC2/src/routes/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import MeuSvg from '../assets/home.svg'
+
+import "./Home.css"
 
 function Home() {
   return (
-    <div>
-        <h1>Home</h1>
+    <div  className="container">
+
+      <p className="texto"> Cursos para <br/> aprender  <br/> de verdade! </p>
+
+      <img src={MeuSvg} alt="Meu SVG" className="imagem" />
+
     </div>
   )
 }
