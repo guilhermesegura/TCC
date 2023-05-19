3668ca439ff013a43d696e4d80b09ee7c2a63838
anguecalmon <calmon.principal@gmail.com>
2023-05-18 22:08:26 -0300

    adiciona atualizações no css e adição da página sobre

--- Guilherme/TCC/src/components/Footer.jsx
+++ Guilherme/TCC/src/components/Footer.jsx
@@ -1,27 +1,24 @@
-import { FaFacebook, FaInstagram, FaGithub } from "react-icons/fa";
+import React from "react";
+import { FaGithub } from "react-icons/fa";
 import styles from "./Footer.module.css";
 
 function Footer() {
   return (
     <div className={styles.div_footer}>
-    <footer className={styles.footer}>
-      <ul className={styles.social_list}>
-        <li>
-          <FaFacebook />
-        </li>
-        <li>
-          <FaInstagram />
-        </li>
-        <li>
-          <FaGithub />
-        </li>
-      </ul>
-      <p className={styles.copy_right}>
-        <span>Ajudatec</span> &copy; 2023
-      </p>
-    </footer>
+      <footer className={styles.footer}>
+        <ul className={styles.social_list}>
+          <li>
+            <a href="https://github.com/anguecalmon/TCC">
+              <FaGithub />
+            </a>
+          </li>
+        </ul>
+        <p className={styles.copy_right}>
+          <span>Ajudatec</span> &copy; 2023
+        </p>
+      </footer>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
